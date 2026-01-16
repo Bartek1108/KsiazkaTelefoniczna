00829e6f6fa714f1fe6d1629c093f238d94b839c
Bartek1108 <62657699+Bartek1108@users.noreply.github.com>
2026-01-16 22:47:45 +0100

    Add files via upload

---
+++ PhoneBook.cs
@@ -0,0 +1,14 @@
+﻿namespace KsiazkaTelefoniczna.Models
+{
+    public class PhoneBook
+    {
+        public int Id { get; set; } 
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Ulica { get; set; }
+        public string NumerMieszkania {  get; set; }
+        public string KodPocztowy { get; set; }
+        public string Miasto { get; set; }
+        public string Telefon {  get; set; }
+    }
+}
