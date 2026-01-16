ddeead4b5c9b6026c08599980e3e2cd993bd1a8b
Bartek1108 <62657699+Bartek1108@users.noreply.github.com>
2026-01-16 22:49:10 +0100

    Add files via upload

---
+++ KsiazkaTelefonicznaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KsiazkaTelefoniczna.Models;
+
+namespace KsiazkaTelefoniczna.Data
+{
+    public class KsiazkaTelefonicznaContext : DbContext
+    {
+        public KsiazkaTelefonicznaContext (DbContextOptions<KsiazkaTelefonicznaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KsiazkaTelefoniczna.Models.PhoneBook> PhoneBook { get; set; } = default!;
+    }
+}
